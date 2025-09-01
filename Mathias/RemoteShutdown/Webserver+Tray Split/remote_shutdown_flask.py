70e441db0eeff2c6b349fd07402caa75ecc1c0e8
Mathias Kornschober <mathias.kornschober@bulme.at>
2025-09-01 19:59:24 +0200

    Added Webp images, Cached img on Browser, and Status Dot

--- Mathias/RemoteShutdown/Webserver+Tray Split/remote_shutdown_flask.py
+++ Mathias/RemoteShutdown/Webserver+Tray Split/remote_shutdown_flask.py
@@ -1,8 +1,13 @@
 import logging
+import mimetypes
 import os
 import socket
 import psutil
-from flask import Flask, render_template_string, redirect, request, render_template, send_from_directory
+from threading import Thread
+import time
+from flask import Flask, render_template_string, redirect, request, render_template, send_from_directory, make_response, jsonify
+
+mimetypes.add_type('image/webp', '.webp')
 
 # ===== Logging =====
 logging.basicConfig(filename="server.log", level=logging.INFO,
@@ -10,18 +15,32 @@ logging.basicConfig(filename="server.log", level=logging.INFO,
 
 app = Flask(__name__)
 PRINT = 0
-
-def customprint(text):
-    if PRINT:
-        print(text)
-    logging.info(text)
+status_cache = {}
 
 # ===== Base directory =====
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 JELLYSEER_DIR = r"D:\Scripts\Mathias\Jellyseerr"
 
+# ===== Name -> Port =====
+PORT_MAPPING = {
+    "sonarr": 8989,
+    "radarr": 7878,
+    "sabnzbd": 6969,
+    "jellyfin": 8096,
+    "jellyseerr": 5055,
+    "plex": 32400,
+    "bazarr": 6767,
+    "tdarr": 8265
+}
+
 def abs_path(relative_path):
     return os.path.join(BASE_DIR, relative_path)
+
+# --- Log
+def customprint(text):
+    if PRINT:
+        print(text)
+    logging.info(text)
 
 # ===== Determine local IP =====
 def get_local_ip():
@@ -35,30 +54,30 @@ def get_local_ip():
         s.close()
     return ip
 
+SERVER_IP = get_local_ip()
+
 # ===== Check if Service is listening on Port (Running/Not running) =====
 def is_port_open(port):
-    import socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.5)
+    sock = socket.socket()
+    sock.settimeout(0.1)
     try:
-        s.connect(("127.0.0.1", port))
-        s.close()
+        sock.connect(("127.0.0.1", port))
         return True
     except:
         return False
+    finally:
+        sock.close()
 
-# ===== Name -> Port =====
-SERVER_IP = get_local_ip()
-PORT_MAPPING = {
-    "sonarr": 8989,
-    "radarr": 7878,
-    "sabnzbd": 6969,
-    "jellyfin": 8096,
-    "jellyseerr": 5055,
-    "plex": 32400,
-    "bazarr": 6767,
-    "tdarr": 8265
-}
+# ===== Update Service Status =====
+def update_status_cache():
+    global status_cache
+    while True:
+        status_cache = {name: is_port_open(port) for name, port in PORT_MAPPING.items()}
+        time.sleep(1)
+
+status_thread = Thread(target=update_status_cache, daemon=True)
+status_thread.start()
+
 
 # ===== Redirect to localhost or <Laptop IP> =====
 def redirect_to_service(service_name):
@@ -142,11 +161,16 @@ def status():
 
 @app.route("/service-status")
 def service_status():
-    status = {}
-    for name, port in PORT_MAPPING.items():
-        status[name] = is_port_open(port)
-    return status
+    return jsonify(status_cache)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/img/<filename>")
+def serve_img(filename):
+    path = os.path.join(app.root_path, "static/img")
+    response = make_response(send_from_directory(path, filename))
+
+    response.headers["Cache-Control"] = "public, max-age=604800" # 604800 sec = 1 Week
+    return response
