a27d836a79cc1670961617b07553be5f35601916
Mathias Kornschober <mathias.kornschober@bulme.at>
2025-09-01 14:20:47 +0200

    RemoteShutdown Webserver complete

--- Mathias/RemoteShutdown/Webserver+Tray Split/remote_shutdown_service.py
+++ Mathias/RemoteShutdown/Webserver+Tray Split/remote_shutdown_service.py
@@ -1,11 +1,17 @@
-#from waitress import serve
-#from remote_shutdown_flask import app
-#
-#if __name__ == "__main__":
-#    serve(app, host="0.0.0.0", port=5000, threads=2)
+## --- R E L E A S E   V E R S I O N  ----
 
-
+from waitress import serve
 from remote_shutdown_flask import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+   serve(app, host="0.0.0.0", port=5000, threads=2)
+
+
+
+
+## --- D E V E L O P M E N T   V E R S I O N  ----
+
+# from remote_shutdown_flask import app
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
