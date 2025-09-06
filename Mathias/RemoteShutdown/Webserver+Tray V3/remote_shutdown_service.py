e42738f5478aa892cea4d613fb0f1894f80e6bae
Mathias Kornschober <mathias.kornschober@bulme.at>
2025-09-06 23:11:22 +0200

    M-RemoteShutdown: Made V3, Start/Stop for all Services

---
+++ Mathias/RemoteShutdown/Webserver+Tray V3/remote_shutdown_service.py
@@ -0,0 +1,29 @@
+## --- R E L E A S E   V E R S I O N  ----
+
+# from waitress import serve
+# from remote_shutdown_flask import app
+
+# if __name__ == "__main__":
+#    serve(app, host="0.0.0.0", port=5000, threads=2)
+
+
+
+
+## --- D E V E L O P M E N T   V E R S I O N  ----
+
+from remote_shutdown_flask import app
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+
+
+
+########################## PY -> EXE ##########################
+
+# -- Tray
+# pyinstaller --onefile --add-data "static;static" --windowed --icon "D:\Scripts\Mathias\_Icons\tray_icon.ico"  "remote_shutdown_tray.py"
+
+# -- Webserver
+# pyinstaller --onefile --add-data "static;static" --windowed --icon "D:\Scripts\Mathias\_Icons\RemoteShutdown.ico" --add-data "templates;templates" "remote_shutdown_service.py"
